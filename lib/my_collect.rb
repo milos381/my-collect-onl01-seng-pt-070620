195fdb507d43117eb65b06bdb6a8a386a2fa330e
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 19:38:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  collection = []
+  i = 0
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]){|name|"#{name}"}
